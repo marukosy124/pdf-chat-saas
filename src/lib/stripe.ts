ef2725b504303df8e26f6f379c506f4312a43751
marukosy124 <soniayeung12435@gmail.com>
2023-11-15 19:46:58 -0500

    fix api version

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -4,8 +4,7 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
-  apiVersion: "2022-11-15",
-  // apiVersion: "2023-10-16",
+  apiVersion: "2023-10-16",
   typescript: true,
 });
 
